7a3673888f9469eafe10c8030f6d176b6628605e
ivanreet507 <ivkaur@calpoly.edu>
2023-10-06 23:38:22 -0700

    Getting users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,10 +38,37 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});    
+const findUserByName = (name) => { 
+   return users['users_list']
+       .filter( (user) => user['name'] === name); 
+}
 
 app.get('/users', (req, res) => {
-    res.send(users);
+   const name = req.query.name;
+   if (name != undefined){
+       let result = findUserByName(name);
+       result = {users_list: result};
+       res.send(result);
+   }
+   else{
+       res.send(users);
+   }
 });
+
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+});  
